8e3df10aadb4e9608e3263abb126bdc402f09893
karuru6225 <e2hjhi@gmail.com>
2016-02-28 12:10:57 +0900

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: ddns
+# Recipe:: default
+#
+# Copyright 1970, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+settings = Chef::EncryptedDataBagItem.load('ddns', 'settings')
+cron 'ddns_register_ip_address' do
+  minute '29'
+  command "/usr/bin/curl -L -u #{settings['ddns_user']}:#{settings['ddns_pass']} #{settings['ddns_url']} > /dev/null 2>&1"
+end
